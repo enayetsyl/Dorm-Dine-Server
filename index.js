5e1181fb7aa90bfc272efe70c6c704d65e468623
Enayet <enayetflweb@gamil.com>
2023-11-24 18:54:46 +0600

    jwt route setup

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 
 const express = require('express');
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
-app.use(cors());
+app.use(cors({
+  origin:[
+    'http://localhost:5173',
+  ],
+  credentials: true
+}));
 app.use(express.json())
+app.use(cookieParser())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ktgpsav.mongodb.net/?retryWrites=true&w=majority`;
@@ -20,9 +28,64 @@ const client = new MongoClient(uri, {
   }
 });
 
+// MIDDLEWARE
+
+
+
+
 async function run() {
   try {
     await client.connect();
+
+    // COLLECTIONS
+    const userCollection = client.db("DormDine").collection("users")
+
+    // AUTH RELATED API
+    app.post('/api/v1/jwt', async(req, res) => {
+      const user = req.body;
+      console.log('user for token2', user)
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1hr'});
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+      })
+      .send({success: true})
+    })
+
+    app.post('/api/v1/logout', async(req, res) => {
+      const user = req.body;
+      res.clearCookie('token', {maxAge: 0}).send({success: true})
+    })
+
+    // GET ROUTE -----------
+
+
+    // POST ROUTE --------------
+    // USER INFO POST ROUTE
+
+    app.post('/api/v1/user', async(req, res) => {
+      const user  = req.body;
+      const existingUser = await userCollection.findOne({email: user.email})
+      if (existingUser) {
+        res.json({message: 'User already registered'})
+      }
+      else {
+        const updatedUser = {
+          ...user,
+          role: 'resident',
+          package: 'none',
+          badge:'bronze',
+        }
+        const result = await userCollection.insertOne(updatedUser)
+        console.log(result)
+        res.send(result)
+      }
+    })
+
+
+   
 
 
     await client.db("admin").command({ ping: 1 });
