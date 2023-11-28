48ffcb2a6fecb46bb3b8c2c27dd0c10ac6dbc374
Enayet <enayetflweb@gamil.com>
2023-11-28 20:15:57 +0600

    stripe payment route setup

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
 const app = express()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -372,6 +373,29 @@ async function run() {
       const query = {_id: new ObjectId(id)}
       const result = await reviewCollection.deleteOne(query)
       res.send(result)
+    })
+
+
+
+    // STRIPE ROUTE
+    // PAYMENT INTENT
+    app.post('/api/v1/create-payment-intent', async(req,res) => {
+      const {price} = req.body;
+      const amount = parseInt(price * 100);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types:['card']
+      },
+      {
+        apiKey: process.env.STRIPE_SECRET_KEY
+      }
+      )
+
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
     })
 
     await client.db("admin").command({ ping: 1 });
