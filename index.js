4015b7a25adfc2ad9d93d49cdaf19c29ef6389e4
Enayet <enayetflweb@gamil.com>
2023-11-28 07:18:40 +0600

    admin review delete route setup

--- index.js
+++ index.js
@@ -320,6 +320,14 @@ async function run() {
       res.send(result)
     })
 
+    // ADMIN REVIEW DELETE ROUTE
+    app.delete('/api/v1/review/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await reviewCollection.deleteOne(query)
+      res.send(result)
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
